13f33539874aff5e860e68431d392d98a8e842b6
dhruv2004rajput-alt <dhruv2004rajput@gmail.com>
2026-04-04 17:55:15 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -101,7 +101,7 @@ export default async function handler(req, res) {
    if (!GEMINI_API_KEY) {
   return res.status(500).json({ error: "API KEY IS MISSING" });
 }
-    const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+     const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
     const response = await fetch(GEMINI_URL, {
       method: "POST",
